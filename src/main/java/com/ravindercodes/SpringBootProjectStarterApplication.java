5727d72fbb6b2c6985175a0083061e6e12b70652
ravindercodes <ravinderfzk06@gmail.com>
2025-02-08 23:02:16 +0530

    feat: Add console log for application startup and port (#12)

--- src/main/java/com/ravindercodes/SpringBootProjectStarterApplication.java
+++ src/main/java/com/ravindercodes/SpringBootProjectStarterApplication.java
@@ -4,6 +4,7 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Slf4j
@@ -13,8 +14,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class SpringBootProjectStarterApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootProjectStarterApplication.class, args);
-        log.info("Application started on port 8080: {}");
+        SpringApplication app = new SpringApplication(SpringBootProjectStarterApplication.class);
+        Environment env = app.run(args).getEnvironment();
+
+        String port = env.getProperty("server.port", "8080");
+        String applicationName = env.getProperty("server.name", "SpringForge");
+
+        log.info("🚀 {} started on port: {}", applicationName, port);
+        System.out.printf("\u001B[32m 🚀 %s started on port: %s \u001B[0m%n", applicationName, port);
     }
 
 }
